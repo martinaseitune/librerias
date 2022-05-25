97fbbd854535e27804dbececb57a104599131646
martinaseitune <105325571+martinaseitune@users.noreply.github.com>
2022-05-25 16:56:22 -0300

    Add files via upload

---
+++ Seitune_Martina_ProyectoFinal/js/index.js
@@ -0,0 +1,241 @@
+//Datos Cliente//
+
+function Cliente (nombre, mail) {
+    this.nombre = nombre;
+    this.mail = mail;
+}
+
+
+//Datos Servicio//
+
+class Datos {
+    
+    constructor(tipoEvento, cantidadPersonas, barraTragos, vajilla, mesaDulce){
+        this.tipoEvento = tipoEvento;
+        this.cantidadPersonas = parseInt(cantidadPersonas);
+        this.barraTragos = barraTragos;
+        this.vajilla = vajilla;
+        this.mesaDulce = mesaDulce;
+    }
+}
+
+
+//Servicios Extra//
+
+class Servicio{
+    constructor(nombre, precioExtra){
+        this.nombre = nombre;
+        this.precioExtra = precioExtra;
+    }
+}
+
+class Cotizacion{
+    constructor(tipo){
+        this.tipo = tipo;
+        this.lista = [];
+        this.total = 0;
+        this.cantidadPersonas = 0;
+        this.precioPersona = 0;
+    }
+
+    definirCantidad(cantidad){
+        this.cantidadPersonas = parseInt(cantidad);
+        this.precioPersona = eleccionEvento(datosDelServicio.tipoEvento);
+        this.total += cantidad * this.precioPersona; 
+    }
+
+    agregarServicio(item){
+        this.lista.push(item);
+        this.total += item.precioExtra * this.cantidadPersonas
+    }
+}
+
+const serviciosContratados = new Cotizacion;
+const datosDelServicio = obtenerDatos();
+
+//Precios//
+
+function eleccionEvento (tipoEvento) {
+    
+    let precioPersona = 0;
+    
+    switch(tipoEvento){
+
+        case "social":
+            if(serviciosContratados.cantidadPersonas <= 100){
+                precioPersona = 4500;
+            } else if (serviciosContratados.cantidadPersonas <= 500){
+                precioPersona = 4000;
+            } else {
+                precioPersona = 3500;
+            }
+            break;
+
+        case "corporativo":
+            if(serviciosContratados.cantidadPersonas <= 100){
+                precioPersona = 5000;
+            } else if (serviciosContratados.cantidadPersonas <= 500){
+                precioPersona = 4500;
+            } else {
+                precioPersona = 4000;
+            }
+            break;
+    }
+    return precioPersona;
+}
+
+
+function agregarExtras(){
+    datosDelServicio.barraTragos == "si" && serviciosContratados.agregarServicio(new Servicio("Barra de Tragos", 4500));
+     
+    datosDelServicio.vajilla == "si" && serviciosContratados.agregarServicio(new Servicio("Vajilla de Color", 2500));
+
+    datosDelServicio.mesaDulce == "si" && serviciosContratados.agregarServicio(new Servicio("Mesa Dulce", 3000));
+    
+}
+
+
+//Obtener datos del form//
+
+function obtenerDatos(){
+    let tipoEvento = document.getElementById("selectEvento").value;
+    let cantidadPersonas = document.getElementById("cantInvitados").value;
+    let barraTragos = document.getElementById("selectTragos").value;
+    let vajilla = document.getElementById("selectVajilla").value;
+    let mesaDulce = document.getElementById("selectDulce").value;
+    
+    let datos = new Datos(tipoEvento, cantidadPersonas, barraTragos, vajilla, mesaDulce);
+    return datos;
+ }
+
+ function obtenerCliente(){
+     let nombreCliente = document.getElementById("nombreCliente").value;
+     let mailCliente = document.getElementById("mailCliente").value;
+
+     let cliente = new Cliente(nombreCliente, mailCliente);
+     return cliente;
+ }
+
+
+
+//Valido datos//
+
+function validarNombre(valor) {
+    return (valor == null || valor.length == 0) ? false : true;
+}
+
+function validarMail(valor) {
+    return (valor == null || valor.length == 0) ? false : true;
+}
+
+function validarInvitados(valor) {
+    return (valor == null || valor.length == 0 || valor == isNaN) ? false : true;
+    
+}
+
+
+
+let formulario = document.getElementById("formPresupuesto");
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+});
+
+function submitForm() {
+
+    guardarCliente(obtenerCliente());
+    
+    serviciosContratados.definirCantidad(document.getElementById("cantInvitados").value);
+    agregarExtras();
+    
+    mostrarPresupuesto();
+
+}
+
+function alert(){
+    Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'Presupuesto creado!',
+        showConfirmButton: false,
+        timer: 1500
+      })
+}
+
+let botonCalcular = document.getElementById("btnCalcular");
+
+botonCalcular.onclick = () => {
+    let nombreValido = validarNombre(document.getElementById("nombreCliente").value);
+    let mailValido = validarMail(document.getElementById("mailCliente").value);
+    let invitadosValido = validarInvitados(document.getElementById("cantInvitados").value);
+
+    if(nombreValido && mailValido && invitadosValido){
+        submitForm();
+        alert();
+    } else{
+        if(nombreValido == false){
+            let nombreFalse = document.getElementById("validar-nombre");
+            nombreFalse.innerHTML = "<h6>Nombre inválido</h6>";
+          }
+          if(mailValido == false){
+            let mailFalse = document.getElementById("validar-mail");
+            mailFalse.innerHTML = "<h6>Mail inválido</h6>";
+          }
+          if(invitadosValido == false){
+            let invitadosFalse = document.getElementById("validar-invitados");
+            invitadosFalse.innerHTML = "<h6>Número inválido</h6>";
+          }
+          return false;
+    }
+}
+
+
+
+function mostrarPresupuesto(){
+
+    let totalPresupuesto = serviciosContratados.total;
+
+    let divPresupuesto = document.createElement("div");
+
+    divPresupuesto.innerHTML = `<h4>Tu presupuesto total es de:</h4>
+                            <h4>$${totalPresupuesto}</h4>`
+
+    let detallePresupuesto = document.getElementById("mostrarPresupuesto");
+
+    detallePresupuesto.append(divPresupuesto);
+
+}
+
+//Local storage
+
+function guardarCliente(cliente) {
+    let jsonCliente = { nombre: cliente.nombre, mail: cliente.mail }
+    let stringifyJson = JSON.stringify(jsonCliente);
+    localStorage.setItem('cliente', stringifyJson);
+}
+  
+cargarCliente();
+  
+function cargarCliente() {
+    let stringifyClient = localStorage.getItem('cliente');
+    let cliente = JSON.parse(stringifyClient);
+    if (cliente != null) {
+        document.getElementById("nombreCliente").value = cliente.nombre;
+        document.getElementById("mailCliente").value = cliente.mail;
+  } 
+  
+}
+
+let botonLimpiar = document.getElementById("btnLimpiar");
+botonLimpiar.onclick = () => {
+    eliminarPresupuesto();
+    localStorage.clear();
+}
+
+
+function eliminarPresupuesto(){
+
+    let eliminarDiv = divPresupuesto;
+    eliminarDiv.remove();
+}
+
+
